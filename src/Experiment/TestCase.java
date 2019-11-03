2d0c76753fccaa5cf04d50f14afe01baa0efed53
moo32 <38340314+moo32@users.noreply.github.com>
2019-11-03 08:36:45 -0500

    added an expirement folder & GUI

--- src/Experiment/TestCase.java
+++ src/Experiment/TestCase.java
@@ -1,3 +1,4 @@
+package Experiment;
 
 public class TestCase {
 
--- src/UI/GUI.java
+++ src/UI/GUI.java
@@ -1,5 +1,24 @@
 package UI;
 
-public class GUI {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+public class GUI extends Application
+{
+	Manager manager = new Manager();
+
+	@Override
+	public void start(Stage stage) 
+	{
+		Pane pane = new Pane();
+		Scene scene = new Scene(pane, 600, 600);
+		stage.setScene(scene);
+		stage.setTitle("Optimization toolset");
+		stage.show();
+		
+	}
+	
+	public static void main(String[] args) {launch(args);}
 }
